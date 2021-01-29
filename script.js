ebb70fddd999cbec3a01041632ab9d8c3f161e24
Renato Xavier <rnatu91@gmail.com>
2021-01-29 12:41:53 -0300

    JS - Criando lógica de inserção de dados no HTML

--- script.js
+++ script.js
@@ -4,3 +4,54 @@ const modal = {
     document.querySelector('.modal-overlay').classList.toggle('active');
   },
 };
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  {
+    id: 1,
+    description: 'Criação website',
+    amount: 500000,
+    date: '13/01/2021',
+  },
+  {
+    id: 1,
+    description: 'Internet',
+    amount: 20000,
+    date: '25/01/2021',
+  },
+];
+
+const Transaction = {
+  incomes() {},
+  expenses() {},
+  total() {},
+};
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = DOM.innetHTMLTransaction(transaction[index]);
+  },
+
+  innetHTMLTransaction(transaction) {
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="expense">${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+
+    return html;
+  },
+};
+
+DOM.addTransaction(transactions, 0);
