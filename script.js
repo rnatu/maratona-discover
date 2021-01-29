451167776ce205b9e7a43cea75e2ff65c5b5ee16
Renato Xavier <rnatu91@gmail.com>
2021-01-29 11:27:18 -0300

    JavaScript - Utilizando toggle

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 const modal = {
-  open() {
-    document.querySelector('.modal-overlay').classList.add('active');
-  },
-
-  close() {
-    document.querySelector('.modal-overlay').classList.remove('active');
+  toggleModal() {
+    document.querySelector('.modal-overlay').classList.toggle('active');
   },
 };
