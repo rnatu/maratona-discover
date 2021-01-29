0e233fb5169b20ebacae7beacdb0883d5137faeb
Renato Xavier <rnatu91@gmail.com>
2021-01-29 15:25:41 -0300

    JS - funcionalidade de formatação da moeda

--- script.js
+++ script.js
@@ -13,15 +13,21 @@ const transactions = [
     date: '23/01/2021',
   },
   {
-    id: 1,
+    id: 2,
     description: 'Criação website',
     amount: 500000,
     date: '13/01/2021',
   },
   {
-    id: 1,
+    id: 3,
     description: 'Internet',
-    amount: 20000,
+    amount: -20510,
+    date: '25/01/2021',
+  },
+  {
+    id: 4,
+    description: 'App',
+    amount: 200000,
     date: '25/01/2021',
   },
 ];
@@ -32,18 +38,41 @@ const Transaction = {
   total() {},
 };
 
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : '';
+
+    value = String(value).replace(/\D/g, '');
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return signal + value;
+  },
+};
+
 const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr');
-    tr.innerHTML = DOM.innetHTMLTransaction(transaction[index]);
+    tr.innerHTML = DOM.innetHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
   },
 
   innetHTMLTransaction(transaction) {
+    const cssClass = transaction.amount > 0 ? 'income' : 'expense';
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="expense">${transaction.amount}</td>
+      <td class=${cssClass}>${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg" alt="Remover transação" />
@@ -54,4 +83,6 @@ const DOM = {
   },
 };
 
-DOM.addTransaction(transactions, 0);
+transactions.forEach((transaction) => {
+  DOM.addTransaction(transaction);
+});
